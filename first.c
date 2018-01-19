14287c3c23f52aed55fa94eda918b840561cbb4f
mnandhini79 <31503915+mnandhini79@users.noreply.github.com>
2018-01-19 15:50:58 +0530

    Create first.c

---
+++ first.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+if(n==0)
+{
+printf("the number is zero");
+}
+if(n>0)
+{
+printf("the number is positive");
+}
+else
+{
+printf("the number is negative");
+}
+return 0;
+}
