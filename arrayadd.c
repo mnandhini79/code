3efe09d9984d813c094a8ecc0e7798f774e398fc
mnandhini79 <31503915+mnandhini79@users.noreply.github.com>
2018-01-21 10:14:20 +0530

    Create arrayadd.c

---
+++ arrayadd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int j,n,a[j];
+	int b,sum=0;
+	printf("enter the array size \n");
+	scanf("%d",&n);
+	for(j=0;j<n;i++)
+	{
+	printf("enter the array values \n");	
+	scanf("%d",&a[j]);
+	}
+	scanf("%d",&b);
+	for(int i=0;i<b;i++)
+	{
+	sum=sum+a[i];
+	}
+	printf("sum %d",sum);
+}
