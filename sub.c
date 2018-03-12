52d3f706a66b41269321077008d936c33ba8a0df
mnandhini79 <31503915+mnandhini79@users.noreply.github.com>
2018-03-12 16:32:50 +0530

    Create sub.c

---
+++ sub.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int m,n;
+    scanf("%d%d",&m,&n);
+    if((m-n)%2==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
